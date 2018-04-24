67c792def644e2a82484cda16d1c1ef1d3affd4a
Daniel Perez Alvarez <danielpa9708@gmail.com>
2018-04-24 10:57:42 -0400

    [typescript] fix typo save-mode-enabled -> save mode-enabled

--- .emacs.d/layers/typescript-layer.el
+++ .emacs.d/layers/typescript-layer.el
@@ -10,7 +10,7 @@
     (interactive)
     (tide-setup)
     (flycheck-mode +1)
-    (setq flycheck-check-syntax-automatically '(save-mode-enabled))
+    (setq flycheck-check-syntax-automatically '(save mode-enabled))
     (eldoc-mode +1)
     (tide-hl-identifier-mode +1)
     (company-mode +1))
