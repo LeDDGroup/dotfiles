93a93e4f534897a5c1a57db8d688fe6fe259dbf3
Daniel Perez Alvarez <danielpa9708@gmail.com>
2018-05-09 10:46:05 -0400

    fix git-timemachine keybingings

--- .emacs.d/layers/git-layer.el
+++ .emacs.d/layers/git-layer.el
@@ -14,6 +14,13 @@
   :commands git-timemachine
   :after (magit))
 
+;; @see https://bitbucket.org/lyro/evil/issue/511/let-certain-minor-modes-key-bindings
+(eval-after-load 'git-timemachine
+  '(progn
+     (evil-make-overriding-map git-timemachine-mode-map 'normal)
+     ;; force update evil keymaps after git-timemachine-mode loaded
+     (add-hook 'git-timemachine-mode-hook #'evil-normalize-keymaps)))
+
 (use-package diff-hl
   :hook (prog-mode . diff-hl-mode))
 
