f1fcfe3ffd344fa86553c762a169a6eb613ebdaa
Daniel Perez Alvarez <danielpa9708@gmail.com>
2018-05-16 12:48:43 -0400

    [floobits] keybindings

--- .emacs.d/layers/floobits/keybindings.el
+++ .emacs.d/layers/floobits/keybindings.el
@@ -1,6 +1,8 @@
 (my-leader-def
   ;; Applications
   "c"  '(:ignore t :which-key "Pair Programming")
-  "cs" '(floobits-share-dir-public :which-key "Share public dir")
+  "cP" '(floobits-share-dir-public :which-key "Share public dir")
   "cp" '(floobits-share-dir-private :which-key "Share private dir")
+  "cs" '(floobits-summon :which-key "Summon everyone to your cursor position")
+  "cl" '(floobits-leave-workspace :which-key "Leave workplace")
   "cj" '(floobits-join-workspace :which-key "Join Workspace"))
