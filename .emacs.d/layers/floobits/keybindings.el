b3537eec4dcd2506d5e7d13ad3513ba9390fa2a2
Daniel Perez Alvarez <danielpa9708@gmail.com>
2018-05-16 12:45:16 -0400

    add some keybindings

---
+++ .emacs.d/layers/floobits/keybindings.el
@@ -0,0 +1,6 @@
+(my-leader-def
+  ;; Applications
+  "c"  '(:ignore t :which-key "Pair Programming")
+  "cs" '(floobits-share-dir-public :which-key "Share public dir")
+  "cp" '(floobits-share-dir-private :which-key "Share private dir")
+  "cj" '(floobits-join-workspace :which-key "Join Workspace"))
