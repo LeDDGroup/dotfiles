ba8f51f79f1fc956661ebbc46951650a4ab5b6bb
Daniel Perez Alvarez <danielpa9708@gmail.com>
2018-05-22 16:04:40 -0400

    evil integration with dired

--- .emacs.d/layers/basic/keybindings.el
+++ .emacs.d/layers/basic/keybindings.el
@@ -139,3 +139,16 @@
 
 (define-key evil-normal-state-map (kbd "j") 'evil-next-visual-line)
 (define-key evil-normal-state-map (kbd "k") 'evil-previous-visual-line)
+
+(eval-after-load 'dired
+  '(progn
+     ;; use the standard Dired bindings as a base
+     (evil-make-overriding-map dired-mode-map 'normal t)
+     (evil-define-key 'normal dired-mode-map
+       "h" 'evil-backward-char
+       "j" 'evil-next-line
+       "k" 'evil-previous-line
+       "l" 'evil-forward-char
+       "J" 'dired-goto-file       ; "j"
+       "K" 'dired-do-kill-lines   ; "k"
+       "r" 'dired-do-redisplay))) ; "l"
